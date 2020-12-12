028934aa8b8b1e9caf195d416840e864413bdbb5
HenryZhuHR <296506195@qq.com>
2020-12-12 10:35:15 +0800

    edit in laptop-matebook14

---
+++ cxx/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/**
+ * @brief 求两数之和
+ * @param a 第一个加数
+ * @param b 第二个加数
+ * @return int 两个数的和
+ */
+int Sum(int a, int b)
+{
+    int sum = a + b; // 两数和
+    return sum;
+}
