d2e0d0b46b21475110bc591c63b8a535f56e7892
HenryZhuHR <296506195@qq.com>
2021-01-04 23:22:15 +0800

    update

---
+++ copy-update-gitee.py
@@ -0,0 +1,14 @@
+
+src_dir='./linux'
+dst_dir='../someTutorials-Linux/linux'
+
+import os
+import shutil
+if os.path.exists(dst_dir):    
+    shutil.rmtree(dst_dir)
+else:
+    print(" --  No such directory: ",dst_dir)
+
+shutil.copytree(src_dir, dst_dir)
+
+# git add .
