5c3ea7b4e2c12b855f6e1d85572f41d4c7a7328c
HenryZhuHR <296506195@qq.com>
2021-01-05 12:54:03 +0800

    update linux

---
+++ DeepLearning/cnn/c.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+img_tensor = tf.constant([[[[1], [2], [3], [1], [0]], [[2], [3], [3], [0],
+                                                       [1]],
+                           [[2], [0], [1], [2], [1]], [[1], [2], [0], [1],
+                                                       [0]],
+                           [[0], [1], [2], [1], [1]]]])
+print(img_tensor)
+filter_tensor = tf.constant([[[[0]], [[1]], [[0]]], [[[1]], [[0]], [[1]]],
+                             [[[0]], [[1]], [[0]]]])
+print(filter_tensor)
+
+result = tf.nn.conv2d(img_tensor,
+                      filter_tensor,
+                      strides=[1, 1, 1, 1],
+                      padding='VALID')
+import pprint
+pprint.pprint(result)
